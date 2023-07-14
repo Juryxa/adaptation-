41397fdb88d2ce3c4311b2947d5cabb702d4a59b
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2023-07-14 21:50:16 +0300

    Delete proj directory

--- proj/content/js/mobile.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('BntNav').onclick = () => {
-  let blocknav = document.getElementById('BlockNav')
-  let navbtn = document.getElementById('BntNav')
-  let scrollblock = document.getElementById("body")
-  blocknav.classList.toggle('block-mobile-nav-active')
-  navbtn.classList.toggle('btn-menu-active')
-  scrollblock.classList.toggle('stop-scrolling');
-}
--- proj/content/js/wiki.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btnwiki').onclick = () => {
-  let blocknav = document.getElementById('BlockWiki')
-  let navbtn = document.getElementById('btnwiki')
-  let scrollblock = document.getElementById("body")
-  blocknav.classList.toggle('box-wiki-links-active')
-  navbtn.classList.toggle('btn-menu-active')
-  scrollblock.classList.toggle('stop-scrolling');
-}
